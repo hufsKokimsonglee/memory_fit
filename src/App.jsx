8af56da24bd0dc3e69a9dbe5e9f2919d4424ca54
K0seoyoung <koseo12@hufs.ac.kr>
2024-06-28 20:36:53 +0900

    header 기초 뿌리, ico, title 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./assets/scss/app.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        {/* <header></header> */}
+        <header>
+          <Header/>
+        </header>
         <main>
           <Routes>
             <Route />
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import '../assets/scss/app.scss';
+
+export default function Header() {
+    return(
+          <div>
+            <div>
+              <h1>온리유</h1>
+            </div>
+            <nav>
+              <ul>
+                <li>홈 화면</li>
+                <li>운동 수업</li>
+              </ul>
+            </nav>
+          </div>
+    );
+}
